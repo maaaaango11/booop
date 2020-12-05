476c3f8679e3a053f7623a934196f91509dd2542
maaaaango11 <44108741+maaaaango11@users.noreply.github.com>
2020-12-05 17:32:45 +0700

    Add files via upload

---
+++ dna/cmake-build-debug/CMakeFiles/dna.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dna.dir/main.cpp.obj"
+  "CMakeFiles/dna.dir/rna.cpp.obj"
+  "dna.exe"
+  "dna.exe.manifest"
+  "dna.pdb"
+  "libdna.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dna.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ dna/main.cpp
@@ -0,0 +1,27 @@
+//#include <string>
+#include <iostream>
+#include "rna.h"
+
+
+
+
+
+//int what(char str, int pos){
+//    if((pos<4)&&(pos>=0)){
+//        str >> 2*(3-pos);
+//        return (str & 0x3);
+//    }
+//}
+//
+int main(){
+    char a = 3;
+    a = a<<6;
+    printf("%d", a);
+//    Nucl a = T;
+//    Nucl b = A;
+//    Rna* r1 = new Rna(a, 10);
+//    Rna* r2 = new Rna(b, 10);
+//    printf("%d\n",r1->getLen());
+//    printf("%d", r1->getCount());
+//    if(r1->isComp(r2)) printf("yes");
+}
