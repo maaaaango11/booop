6f03a0ee2edd228aab9640787049c516fc655576
maaaaango <maaaaango@mail.ru>
2021-01-25 15:43:13 +0700

    new1

--- life/Commando.cpp
+++ life/Commando.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //
 // Created by МаксиМ on 19.11.2020.
 //
@@ -29,35 +28,4 @@ std::string Commando::sendCommand(){
 }
 Args* Commando::sendArgs(){
     return args;
-=======
-//
-// Created by МаксиМ on 19.11.2020.
-//
-
-#include "Commando.h"
-
-Commando::Commando() {
-    int (Commando::*resetPrt)(Args) = &Commando::reset;
-    int (Commando::*setPrt)(Args) = &Commando::set;
-    int (Commando::*clearPrt)(Args) = &Commando::clear;
-    int (Commando::*stepPrt)(Args) = &Commando::step;
-    int (Commando::*backPrt)(Args) = &Commando::back;
-    int (Commando::*savePrt)(Args) = &Commando::save;
-    int (Commando::*loadPrt)(Args) = &Commando::load;
-    commands.push_back(std::make_pair("reset",resetPrt));
-    commands.push_back(std::make_pair("set",setPrt));
-    commands.push_back(std::make_pair("clear",clearPrt));
-    commands.push_back(std::make_pair("step",stepPrt));
-    commands.push_back(std::make_pair("back",backPrt));
-    commands.push_back(std::make_pair("save",savePrt));
-    commands.push_back(std::make_pair("load",loadPrt));
-}
-
-void Commando::getCommand() {
-    std::string command;
-    std::cin >> command;
-    if(command.length() != 0){
-
-    }
->>>>>>> 476c3f8679e3a053f7623a934196f91509dd2542
 }
